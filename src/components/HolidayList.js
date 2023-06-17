2c82003de93f94e09f27e53e2c3bf1fd48a2638e
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-17 18:26:50 +0530

    submitting code-Sat Jun 17 2023 18:26:48 GMT+0530 (India Standard Time)

--- src/components/HolidayList.js
+++ src/components/HolidayList.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const HolidayList = (props)=>{
+    let k=0;
     return(
         <>
             <ol>
                 {props.city.map((city)=>{
-                    if(city.country==='India')
-                    return <li>{city.name}</li>
+                    
+                    if(city.country==='India'){
+                    return <li key={"location"+(++k)}>{city.name}</li>
+                    }
                 })}
             </ol>
         </>
